5a11c6f5b4a64aa40b3f3569b9885a4f026d9abf
prodoudou <sin8118112001@yahoo.com.hk>
2023-10-06 16:02:21 +0800

    update

---
+++ frontend-vue/demo-coin/src/test/java/com/example/coin/democoin/DemoCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coin.democoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
